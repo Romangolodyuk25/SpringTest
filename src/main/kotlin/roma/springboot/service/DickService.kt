377cbe8a04a221b450a2dbb9e82c0014abe0d357
Romangolodyuk25 <112016931+Romangolodyuk25@users.noreply.github.com>
2022-11-30 15:40:23 +0300

    Изменил логику c учетом возраста

--- src/main/kotlin/roma/springboot/service/DickService.kt
+++ src/main/kotlin/roma/springboot/service/DickService.kt
@@ -7,12 +7,22 @@ import roma.springboot.dto.DickStatusResponse
 @Service
 class DickService {
     fun isDickOk(dickStatusRequest: DickStatusRequest): DickStatusResponse {
-        if (dickStatusRequest.dickLength < 10) {
-            return DickStatusResponse("${dickStatusRequest.name} тебе не повезло в ${dickStatusRequest.age} иметь такой короткий член!")
-        } else if (dickStatusRequest.dickLength > 10 && dickStatusRequest.dickLength < 15) {
-            return DickStatusResponse("${dickStatusRequest.name} в ${dickStatusRequest.age} это норм, расслабься!")
-        } else {
-            return DickStatusResponse("${dickStatusRequest.name} нихуя у тебя труба!")
+
+        if ((dickStatusRequest.age < 10) && (dickStatusRequest.dickLength < 10)) {
+            return DickStatusResponse("${dickStatusRequest.name}, для  ${dickStatusRequest.age} пойдет")
+        } else if ((dickStatusRequest.age < 10) && (dickStatusRequest.dickLength > 15) && (dickStatusRequest.dickLength < 20) || (dickStatusRequest.dickLength > 20)) {
+            return DickStatusResponse("${dickStatusRequest.name} для твоих ${dickStatusRequest.age}лет ты мутант")
+        } else if ((dickStatusRequest.age > 10) && (dickStatusRequest.dickLength < 10)) {
+            return DickStatusResponse("${dickStatusRequest.name}, для ${dickStatusRequest.age} тебе не повезло")
+        } else if ((dickStatusRequest.age > 10) && (dickStatusRequest.age < 15) && (dickStatusRequest.dickLength > 10) && (dickStatusRequest.dickLength < 15)) {
+            return DickStatusResponse("${dickStatusRequest.name} для твоего ${dickStatusRequest.age} все вроде ок")
+        } else if ((dickStatusRequest.age > 15) && (dickStatusRequest.age < 20) && (dickStatusRequest.dickLength < 10)) {
+            return DickStatusResponse("${dickStatusRequest.name} для твоего ${dickStatusRequest.age} все плохо")
+        } else if ((dickStatusRequest.age > 15) && (dickStatusRequest.age < 20) && (dickStatusRequest.dickLength > 15) && (dickStatusRequest.dickLength < 20)) {
+            return DickStatusResponse("${dickStatusRequest.name} для твоего ${dickStatusRequest.age} заебумба")
+        } else if ((dickStatusRequest.age > 15) && (dickStatusRequest.age < 20) && (dickStatusRequest.dickLength > 20)) {
+            return DickStatusResponse("${dickStatusRequest.name} для ${dickStatusRequest.age} это труба ")
         }
+        return DickStatusResponse("${dickStatusRequest.name} не пиши сюда больше")
     }
 }
